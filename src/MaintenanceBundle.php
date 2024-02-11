e69427f24bfc55ee90d722c414f64bcd734cd4a9
Mykyta Melnyk <wawoxe@proton.me>
2024-02-11 22:40:21 +0100

    Initialized Bundle

---
+++ src/MaintenanceBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * (c) Mykyta Melnyk <wawoxe@proton.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Wawoxe\MaintenanceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * @author Mykyta Melnyk <wawoxe@proton.me>
+ */
+final class MaintenanceBundle extends AbstractBundle
+{
+}
